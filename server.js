5368247a7d63d97572228bda1e69efc850276ea7
Jose Salazar <jsalazar@pneumarespiratory.com>
2024-01-15 11:56:55 -0500

    created the base API and used insomnia to test it

--- server.js
+++ server.js
@@ -2,10 +2,25 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 
+app.use( express.json() )
+
 app.get('/tshirt',(req,res) => {
     res.status(200).send({
         tshirt: 'red shirt',
         size: 'large'
+    })
+})
+
+app.post('/tshirt/:id', (req,res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+    
+    if(!logo) {
+        res.status(418).send({ message: 'We need a logo!' })
+    }
+
+    res.send({
+        thirt: `Red Shirt with your ${logo} and ID of ${id}` 
     })
 })
 
